7382dfdc155898dfc31571376d8db01151f20878
johnnynode <johnnynode@gmail.com>
2017-10-05 21:17:20 +0800

    add CommnetBox component

---
+++ app/comment/CommentBox.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import React from "react";
+
+class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="ui comments"></div>
+        );
+    }
+}
