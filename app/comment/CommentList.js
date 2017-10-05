543ef1fb2a43cccca1b209d8dd02a1afc2ee8f98
johnnynode <johnnynode@gmail.com>
2017-10-05 21:57:35 +0800

    add Comment componet and use it in Commnetlist

--- app/comment/CommentList.js
+++ app/comment/CommentList.js
@@ -1,11 +1,17 @@
 "use strict";
 
 import React from "react";
+import Comment from "./Comment";
 
 class CommentList extends React.Component {
     render() {
         return (
-            <div>评论列表</div>
+            <div>
+                <h3>评论列表</h3>
+                <Comment author="Johnny" date="3分钟前">天气不错啊！</Comment>
+                <Comment author="Tony" date="5分钟前">天气不错啊！</Comment>
+                <Comment author="Lucy" date="12分钟前">除去走走！</Comment>
+            </div>
         );
     }
 }
