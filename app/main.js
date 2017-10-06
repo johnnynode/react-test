9151a93b11c7e679ee748b4dbf67c116cd0573cf
johnnynode <johnnynode@gmail.com>
2017-10-06 19:51:09 +0800

    add css! comment

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import 'semantic-ui/semantic.min.css!'
+import 'semantic-ui/semantic.min.css!' // 导入css注意最后的!一定要有
 import React from "react";
 import ReactDom from "react-dom";
 import CommentBox from "./comment/CommentBox";
