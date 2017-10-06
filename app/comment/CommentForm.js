e426c805d84246f2685fac2100c679ad2a415ba1
johnnynode <johnnynode@gmail.com>
2017-10-06 21:06:08 +0800

    using refs to get dom element

--- app/comment/CommentForm.js
+++ app/comment/CommentForm.js
@@ -6,15 +6,18 @@ class CommentForm extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
         console.log("提交表单");
+        // 在组件中使用refs访问页面上的元素
+        console.log(this.refs.author.value, this.refs.text.value);
     }
+
     render() {
         return (
-            <form className="ui reply form" onSubmit={this.handleSubmit}>
+            <form className="ui reply form" onSubmit={this.handleSubmit.bind(this)}>
                 <div className="field">
-                    <input type="text" placeholder="姓名"/>
+                    <input type="text" placeholder="姓名" ref="author"/>
                 </div>
                 <div className="field">
-                    <textarea placeholder="评论"></textarea>
+                    <textarea placeholder="评论" ref="text"></textarea>
                 </div>
                 <button type="submit" className="ui blue button">
                     添加评论
